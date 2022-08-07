cdf689a48335d3be15540f33730b0e9c8b02fa72
Skiddzie <aidanthe1@gmail.com>
2022-08-06 21:12:26 -0400

    starter code init commit

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Footer = () => {
+  return (
+    <footer className="w-100 mt-auto bg-secondary p-4">
+      <div className="container">&copy;2020 by Skiddzie</div>
+    </footer>
+  );
+};
+
+export default Footer;
