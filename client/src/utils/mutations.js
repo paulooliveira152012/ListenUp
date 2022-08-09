1d9ce272db0926e06f4a6a9a605ad3e55812fd77
Derricka S <derrickas728@gmail.com>
2022-08-09 18:53:45 -0400

    remove merge conflicts

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -25,36 +25,19 @@ export const ADD_USER = gql`
 `;
 
 export const ADD_ARTIST = gql`
-<<<<<<< HEAD
   mutation addArtist($name: String!, $wiki: String, $description: String) {
     addArtist(name: $name, wiki: $wiki, description: $description) {
       _id
       name
       wiki
       description
-=======
-  mutation addArtist($name: String!, $wiki: String!, $description: String!) {
-    addArtist(name: $name, wiki: $wiki, description: $description) {
-      token
-      artist {
-        _id
-        name
-        wiki
-        description
-      }
->>>>>>> 3f0c7ecf42685a553e2da8896604e0223af32467
     }
   }
 `;
 
 export const ADD_THOUGHT = gql`
-<<<<<<< HEAD
   mutation addThought($thoughtText: String!, $artistName: String!, $artistId: ID!) {
     addThought(thoughtText: $thoughtText, artistName: $artistName, artistId: $artistId) {
-=======
-  mutation addThought($thoughtText: String!, $artistId: ID!) {
-    addThought(thoughtText: $thoughtText, artistId: $artistId) {
->>>>>>> 3f0c7ecf42685a553e2da8896604e0223af32467
       _id
       artistName
       thoughtText
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -13,16 +13,9 @@ const typeDefs = gql`
 
   type Artist {
     _id: ID
-<<<<<<< HEAD
     name: String,
     wiki: String,
     description: String,
-=======
-    name: String
-    wiki: String
-    description: String
-    fanCount: Int
->>>>>>> 3f0c7ecf42685a553e2da8896604e0223af32467
     thoughts: [Thought]
     fans: [User]
   }
@@ -62,17 +55,10 @@ const typeDefs = gql`
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-<<<<<<< HEAD
     addThought(thoughtText: String!, artistName: String, artistId: ID!): Thought
     addReaction(thoughtId: ID!, reactionBody: String!): Thought
     addFriend(friendId: ID!): User
     addArtist(name: String!, wiki: String, description: String): Artist
-=======
-    addThought(thoughtText: String!, artistId: ID!): Thought
-    addReaction(thoughtId: ID!, reactionBody: String!): Thought
-    addFriend(friendId: ID!): User
-    addArtist(name: String!, wiki: String!, description: String!): Artist
->>>>>>> 3f0c7ecf42685a553e2da8896604e0223af32467
     addFan(artistId: ID!, userId: ID!): Artist
     removeFan(artistId: ID!, userId: ID!): Artist
   }
